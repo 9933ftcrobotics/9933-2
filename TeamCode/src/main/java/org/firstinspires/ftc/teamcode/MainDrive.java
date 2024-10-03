7ab343703e8db3b1cdc6aa30b3e3268e4761d1d5
Team 9933 FTC <55024397+9933ftcrobotics@users.noreply.github.com>
2024-10-03 00:35:22 -0400

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainDrive.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.hardware.RevIMU;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
+
+@TeleOp
+public class MainDrive extends LinearOpMode {
+
+    // This variable determines whether the following program
+    // uses field-centric or robot-centric driving styles. The
+    // differences between them can be read here in the docs:
+    // https://docs.ftclib.org/ftclib/features/drivebases#control-scheme
+    static final boolean FIELD_CENTRIC = true;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // constructor takes in frontLeft, frontRight, backLeft, backRight motors
+        // IN THAT ORDER
+
+        DriveSubsystem drive = new DriveSubsystem(
+                new Motor(hardwareMap, "leftFront", Motor.GoBILDA.RPM_312),
+                new Motor(hardwareMap, "rightFront", Motor.GoBILDA.RPM_312),
+                new Motor(hardwareMap, "leftRear", Motor.GoBILDA.RPM_312),
+                new Motor(hardwareMap, "rightRear", Motor.GoBILDA.RPM_312),
+                new RevIMU(hardwareMap)
+        );
+
+        // This is the built-in IMU in the REV hub.
+        // We're initializing it by its default parameters
+        // and name in the config ('imu'). The orientation
+        // of the hub is important. Below is a model
+        // of the REV Hub and the orientation axes for the IMU.
+        //
+        //                           | Z axis
+        //                           |
+        //     (Motor Port Side)     |   / X axis
+        //                       ____|__/____
+        //          Y axis     / *   | /    /|   (IO Side)
+        //          _________ /______|/    //      I2C
+        //                   /___________ //     Digital
+        //                  |____________|/      Analog
+        //
+        //                 (Servo Port Side)
+        //
+        // (unapologetically stolen from the road-runner-quickstart)
+
+
+        // the extended gamepad object
+        GamepadEx driverOp = new GamepadEx(gamepad1);
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+
+            drive.drive(driverOp.getLeftX(), driverOp.getLeftY(), driverOp.getRightX(), true);
+
+        }
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -0,0 +1,113 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.hardware.GyroEx;
+import com.arcrobotics.ftclib.hardware.RevIMU;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.Motor.Encoder;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.DifferentialDriveKinematics;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.DifferentialDriveOdometry;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.DifferentialDriveWheelSpeeds;
+
+import org.firstinspires.ftc.teamcode.DriveConstants;
+
+public class DriveSubsystem extends SubsystemBase {
+
+    private Motor leftFront, rightFront, rightRear, leftRear;
+    private Encoder leftEncoder, rightEncoder;
+    private GyroEx gyro;
+    private DifferentialDriveOdometry odometry;
+    private DifferentialDriveKinematics kinematics;
+    private MecanumDrive drive;
+    private RevIMU imu;
+
+    public DriveSubsystem(Motor leftFront, Motor rightFront, Motor rightRear, Motor leftRear, RevIMU imu) {
+        leftFront.setInverted(true);
+        leftRear.setInverted(true);
+        rightFront.setInverted(false);
+        rightRear.setInverted(false);
+        drive = new MecanumDrive(false,
+                leftFront, rightFront, leftRear, rightRear
+        );
+        this.imu = imu;
+        imu.init();
+
+        kinematics = new DifferentialDriveKinematics(DriveConstants.TRACK_WIDTH);
+    }
+
+    @Override
+    public void periodic() {
+        odometry.update(gyro.getRotation2d(), leftEncoder.getDistance(), rightEncoder.getDistance());
+    }
+
+    public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+        return new DifferentialDriveWheelSpeeds(leftEncoder.getRate(), rightEncoder.getRate());
+    }
+
+    public Pose2d getCurrentPose() {
+        return odometry.getPoseMeters();
+    }
+
+    public void drive(double LeftX, double LeftY, double RightX, boolean FIELD_CENTRIC) {
+        // Driving the mecanum base takes 3 joystick parameters: leftX, leftY, rightX.
+        // These are related to the left stick x value, left stick y value, and
+        // right stick x value respectively. These values are passed in to represent the
+        // strafing speed, the forward speed, and the turning speed of the robot frame
+        // respectively from [-1, 1].
+
+        if (!FIELD_CENTRIC) {
+
+            // For a robot centric model, the input of (0,1,0) for (leftX, leftY, rightX)
+            // will move the robot in the direction of its current heading. Every movement
+            // is relative to the frame of the robot itself.
+            //
+            //                 (0,1,0)
+            //                   /
+            //                  /
+            //           ______/_____
+            //          /           /
+            //         /           /
+            //        /___________/
+            //           ____________
+            //          /  (0,0,1)  /
+            //         /     ↻     /
+            //        /___________/
+
+            // optional fourth parameter for squared inputs
+            drive.driveRobotCentric(
+                    LeftX,
+                    LeftY,
+                    RightX,
+                    false
+            );
+        } else {
+
+            // Below is a model for how field centric will drive when given the inputs
+            // for (leftX, leftY, rightX). As you can see, for (0,1,0), it will travel forward
+            // regardless of the heading. For (1,0,0) it will strafe right (ref to the 0 heading)
+            // regardless of the heading.
+            //
+            //                   heading
+            //                     /
+            //            (0,1,0) /
+            //               |   /
+            //               |  /
+            //            ___|_/_____
+            //          /           /
+            //         /           / ---------- (1,0,0)
+            //        /__________ /
+
+            // optional fifth parameter for squared inputs
+            drive.driveFieldCentric(
+                    LeftX,
+                    LeftY,
+                    RightX,
+                    imu.getRotation2d().getDegrees(),   // gyro value passed in here must be in degrees
+                    false
+            );
+        }
+    }
+
+}
