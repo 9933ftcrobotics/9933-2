8c7f961e2ed2b6390592dfa2a9fe1e163ab97106
Team 9933 FTC <55024397+9933ftcrobotics@users.noreply.github.com>
2024-12-01 12:15:14 -0500

    12/01/24 New Auto For New End Effector

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MyClass.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MyClass.java
@@ -8,7 +8,7 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MyClass {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(700);
+        MeepMeep meepMeep = new MeepMeep(450);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
@@ -16,13 +16,64 @@ public class MyClass {
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-15, 62, Math.toRadians(-90)))
+
                 .strafeTo(new Vector2d(-5, 36))
+                //Step, Score
+
+                .strafeTo(new Vector2d(-5, 45))
+                //Step, Finish Score
+
+                .strafeTo(new Vector2d(-48, 40))
+                //Step, Rest
+
+                //Step, Pick
+
+                .turnTo(Math.toRadians(90))
+                .strafeTo(new Vector2d(-58, 50))
+                //Step, Rest
+
+                //Step, Dispense
+
+                .strafeTo(new Vector2d(-47, 45))
+                .strafeTo(new Vector2d(-47, 52))
+                //Step, Rest
+
+                //Step, Pick Up
+
+                .strafeTo(new Vector2d(-5, 50))
+                .turnTo(Math.toRadians(-90))
+                //Step, Rest
+
+                .strafeTo(new Vector2d(-5, 36))
+                //Step, Score
+
+                .strafeTo(new Vector2d(-5, 45))
+                //Step, Finish Score
+
+                .strafeTo(new Vector2d(-47, 45))
+                .turnTo(Math.toRadians(90))
+                //Step, Rest
+
+                .strafeTo(new Vector2d(-47, 52))
+                //Step, Pick Up
+
+                .strafeTo(new Vector2d(-5, 50))
+                .turnTo(Math.toRadians(-90))
+                //Step, Rest
+
+                .strafeTo(new Vector2d(-5, 36))
+                //Step, Score
+
+                .strafeTo(new Vector2d(-5, 45))
+                //Step, Rest
+
+                /*.strafeTo(new Vector2d(-5, 36))
                 .strafeTo(new Vector2d(-5, 45))
                 .splineToLinearHeading(new Pose2d(-36, 25, Math.toRadians(180)), 5)
                 .lineToYSplineHeading(55, Math.toRadians(90))
                 .turnTo(Math.toRadians(180))
-                                .strafeTo(new Vector2d(-45, 25))
-                        .turnTo(Math.toRadians(90))
+                .strafeTo(new Vector2d(-45, 25))
+                .turnTo(Math.toRadians(90))
                 .strafeTo(new Vector2d(-58, 50))
                 .strafeTo(new Vector2d(-45, 45))
                 .strafeTo(new Vector2d(-45, 55))
@@ -30,23 +81,7 @@ public class MyClass {
                                 .turnTo(Math.toRadians(-90))
                 .strafeTo(new Vector2d(-5, 36))
                 .strafeTo(new Vector2d(-5, 45))
-                .strafeTo(new Vector2d(-47, 55))
-                /*.strafeTo(new Vector2d(-36, 45))
-                .strafeTo(new Vector2d(-46, 46))
-                        .turnTo(Math.toRadians(90))
-                .strafeTo(new Vector2d(-50, 50))
-                .turnTo(Math.toRadians(-90))
-                .strafeTo(new Vector2d(-54, 46))
-                .strafeTo(new Vector2d(-58, 46))
-                .turnTo(Math.toRadians(90))
-                .strafeTo(new Vector2d(-60, 50))
-                .strafeTo(new Vector2d(-50, 45))
-                .strafeTo(new Vector2d(-50, 50))
-                .strafeTo(new Vector2d(-5, 45))
-                .turnTo(Math.toRadians(-90))
-                .strafeTo(new Vector2d(-5, 36))
-                .strafeTo(new Vector2d(-5, 45))
-                .strafeTo(new Vector2d(-53, 58))*/
+                .strafeTo(new Vector2d(-47, 55))*/
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
